ef2d22d4143c9793ab5f40a50223292c32737c58
emma <emma.shymilovuch@gmail.com>
2015-08-06 22:08:17 +0300

    Homework #13.1 (bat files)

---
+++ addressbook/group-data-generator.bat
@@ -0,0 +1,2 @@
+java -cp out\production\addressbook;d:\project\tools\xstream-1.4.8\lib\xstream-1.4.8.jar;d:\project\tools\xstream-1.4.8\lib\xstream\xpp3_min-1.1.4c.jar;d:\project\tools\xstream-1.4.8\lib\xstream\xmlpull-1.1.3.1.jar;d:\project\tools\selenium-server-standalone-2.46.0.jar com.example.tests.GroupDataGenerator 5 groups.xml xml
+pause
